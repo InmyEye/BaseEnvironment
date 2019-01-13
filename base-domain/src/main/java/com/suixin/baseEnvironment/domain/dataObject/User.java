38326369d7cddfdd4b5590259482578f266ea0cb
suixin <1123541915@qq.com>
2019-01-13 16:44:46 +0800

    配置thymleaf，springbootTest，mybatis逆向支持

---
+++ base-domain/src/main/java/com/suixin/baseEnvironment/domain/dataObject/User.java
@@ -0,0 +1,53 @@
+package com.suixin.baseEnvironment.domain.dataObject;
+
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description == null ? null : description.trim();
+    }
+}
